a297c23c0ae063174941a43aeddba9ed49e92441
Silv4b <brunno.linkin@gmail.com>
2021-09-09 13:49:20 -0300

    Add dark/light theme toggle

---
+++ src/components/Toggle/Toggle.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <input
+      @change="toggleTheme"
+      id="checkbox"
+      type="checkbox"
+      class="switch-checkbox"
+    />
+    <label for="checkbox" class="switch-label">
+      <span>🌙</span>
+      <span>☀️</span>
+      <div
+        class="switch-toggle"
+        :class="{ 'switch-toggle-checked': userTheme === 'dark-theme' }"
+      ></div>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const initUserTheme = this.getMediaPreference();
+    this.setTheme(initUserTheme);
+  },
+
+  data() {
+    return {
+      userTheme: "light-theme",
+    };
+  },
+
+  methods: {
+    toggleTheme() {
+      const activeTheme = localStorage.getItem("user-theme");
+      if (activeTheme === "light-theme") {
+        this.setTheme("dark-theme");
+      } else {
+        this.setTheme("light-theme");
+      }
+    },
+
+    setTheme(theme) {
+      localStorage.setItem("user-theme", theme);
+      this.userTheme = theme;
+      document.documentElement.className = theme;
+    },
+
+    getMediaPreference() {
+      const hasDarkPreference = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      if (hasDarkPreference) {
+        return "dark-theme";
+      } else {
+        return "light-theme";
+      }
+    },
+  },
+};
+</script>
+
+<style>
+@import "../Toggle/Toggle.style.scss";
+</style>
