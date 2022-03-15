20075fca89cd5eb2d69c374e31c2f23087bafd2a
Bruno Silva <brunosilva@viggosistemas.com.br>
2022-03-15 20:28:36 -0300

    fix: only tabs on the code :v

--- src/components/Squares/Squares.vue
+++ src/components/Squares/Squares.vue
@@ -1,109 +1,118 @@
 <template>
-	<div class="master-border">
-		<div class="container">
-			<!-- TOGGLE -->
-			<div class="container toggle">
-				<Toggle />
-			</div>
-			<!--  PERFIL -->
-			<div class="container">
-				<div class="profile"></div>
-			</div>
+  <div class="master-border">
+    <div class="container">
+      <!-- TOGGLE -->
+      <div class="container toggle">
+        <Toggle />
+      </div>
+      <!--  PERFIL -->
+      <div class="container">
+        <div class="profile"></div>
+      </div>
 
-			<!-- TEXTOS -->
-			<div class="container">
-				<div class="text-name">{{ person.title }}</div>
-				<div class="text-job">{{ person.job }}</div>
-			</div>
+      <!-- TEXTOS -->
+      <div class="container">
+        <div class="text-name">{{ person.title }}</div>
+        <div class="text-job">{{ person.job }}</div>
+      </div>
 
-			<!-- REDES SOCIAIS -->
-			<div class="container-row">
-				<div v-for="sl in socialList" v-bind:key="sl.id" :class="classCard" @click="openLink(sl.href)">
-					<a :href="sl.href" target="_blank" :class="classSocialIcons">
-						<i :class="sl.iclass"></i>
-					</a>
-				</div>
-			</div>
+      <!-- REDES SOCIAIS -->
+      <div class="container-row">
+        <div
+          v-for="sl in socialList"
+          v-bind:key="sl.id"
+          :class="classCard"
+          @click="openLink(sl.href)"
+        >
+          <a :href="sl.href" target="_blank" :class="classSocialIcons">
+            <i :class="sl.iclass"></i>
+          </a>
+        </div>
+      </div>
 
-			<!-- MODAL PLAYLISTS SPOTIFY -->
-			<div class="container">
-				<div type="button" @click="$refs.modalName.openModal()" class="spotify-button">
-					<div class="container-row">
-						<a class="social_icon_style_2">
-							<i class="bx bxs-playlist"></i>
-						</a>
-						<div class="change_later"></div>
-					</div>
-				</div>
-				<modal ref="modalName">
-					<template v-slot:header>
-						<h1>Playlists</h1>
-					</template>
-					<template v-slot:body>
-						<div class="container font-card-color">
-							<div
-								v-for="pl in playList"
-								v-bind:key="pl.id"
-								class="flex left spotify-pl-card"
-								@click="openLink(pl.link)"
-							>
-								<div :class="pl.cover"></div>
-								<div class="spotify-pl-container">
-									<div class="spotify-pl-title">{{ pl.nome }}</div>
-									<div class="spotify-pl-desc">{{ pl.desc }}</div>
-								</div>
-							</div>
-						</div>
-					</template>
-				</modal>
-			</div>
+      <!-- MODAL PLAYLISTS SPOTIFY -->
+      <div class="container">
+        <div
+          type="button"
+          @click="$refs.modalName.openModal()"
+          class="spotify-button"
+        >
+          <div class="container-row">
+            <a class="social_icon_style_2">
+              <i class="bx bxs-playlist"></i>
+            </a>
+            <div class="change_later"></div>
+          </div>
+        </div>
+        <modal ref="modalName">
+          <template v-slot:header>
+            <h1>Playlists</h1>
+          </template>
+          <template v-slot:body>
+            <div class="container font-card-color">
+              <div
+                v-for="pl in playList"
+                v-bind:key="pl.id"
+                class="flex left spotify-pl-card"
+                @click="openLink(pl.link)"
+              >
+                <div :class="pl.cover"></div>
+                <div class="spotify-pl-container">
+                  <div class="spotify-pl-title">{{ pl.nome }}</div>
+                  <div class="spotify-pl-desc">{{ pl.desc }}</div>
+                </div>
+              </div>
+            </div>
+          </template>
+        </modal>
+      </div>
 
-			<!-- TEXTO E LOGO -->
-			<div class="container">
-				<div class="text-bottom">{{ person.desc }}</div>
-				<div class="logo-img">
-					<img src="@/assets/logo.svg" />
-				</div>
-			</div>
-		</div>
-	</div>
+      <!-- TEXTO E LOGO -->
+      <div class="container">
+        <div class="text-bottom">{{ person.desc }}</div>
+        <div class="logo-img">
+          <img src="@/assets/logo.svg" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import Modal from "../Modal/Modal.vue";
 import Toggle from "../Toggle/Toggle.vue";
 import {
-	about,
-	social,
-	spotifyplaylists,
-	imageFooter,
+  about,
+  social,
+  spotifyplaylists,
+  imageFooter,
 } from "../../../properties.js";
 
 export default {
-	components: {
-		Modal,
-		Toggle,
-		/* eslint-disable vue/no-unused-components */
-		about,
-		social,
-		spotifyplaylists,
-		imageFooter,
-	},
-	methods: {
-		openLink: function (link) {
-			window.open(link, "_blank");
-		}
-	},
-	data() {
-		return {
-			person: about.person,
-			playList: spotifyplaylists,
-			socialList: social,
-			imageFooter: imageFooter,
-			classCard: "social center",
-			classSocialIcons: "social_icon_style",
-		};
-	},
+  components: {
+    Modal,
+    Toggle,
+    /* eslint-disable vue/no-unused-components */
+    about,
+    social,
+    spotifyplaylists,
+    imageFooter,
+  },
+  methods: {
+    openLink: function (link) {
+      window.open(link, "_blank");
+    },
+  },
+  data() {
+    return {
+      person: about.person,
+      playList: spotifyplaylists,
+      socialList: social,
+      imageFooter: imageFooter,
+      classCard: "social center",
+      classSocialIcons: "social_icon_style",
+    };
+  },
 };
 </script>
 
--- src/components/Toggle/Toggle.vue
+++ src/components/Toggle/Toggle.vue
@@ -1,67 +1,76 @@
 <!-- TOGGLE -->
 <template>
-	<div>
-		<vue-scroll-progress-bar
-			height="0.3rem"
-			zIndex="999"
-			backgroundColor="linear-gradient(to right, var(--white-color), var(--white-color))"
-		/>
-		<div class="container">
-			<input @change="toggleTheme" id="checkbox" type="checkbox" class="switch-checkbox" zIndex="999" />
-			<label for="checkbox" class="switch-label">
-				<span>🌙</span>
-				<span>☀️</span>
-				<div class="switch-toggle" :class="{ 'switch-toggle-checked': userTheme === 'dark-theme' }"></div>
-			</label>
-		</div>
-	</div>
+  <div>
+    <vue-scroll-progress-bar
+      height="0.3rem"
+      zIndex="999"
+      backgroundColor="linear-gradient(to right, var(--white-color), var(--white-color))"
+    />
+    <div class="container">
+      <input
+        @change="toggleTheme"
+        id="checkbox"
+        type="checkbox"
+        class="switch-checkbox"
+        zIndex="999"
+      />
+      <label for="checkbox" class="switch-label">
+        <span>🌙</span>
+        <span>☀️</span>
+        <div
+          class="switch-toggle"
+          :class="{ 'switch-toggle-checked': userTheme === 'dark-theme' }"
+        ></div>
+      </label>
+    </div>
+  </div>
 </template>
 
 <script>
 import { VueScrollProgressBar } from "@guillaumebriday/vue-scroll-progress-bar";
 
 export default {
-	name: "Toggle",
-	components: {
-		VueScrollProgressBar,
-	},
-	mounted() {
-		const initUserTheme = this.getMediaPreference();
-		this.setTheme(initUserTheme);
-	},
-	data() {
-		return {
-			userTheme: "light-theme",
-		};
-	},
-	methods: {
-		toggleTheme() {
-			const activeTheme = localStorage.getItem("user-theme");
-			if (activeTheme === "light-theme") {
-				this.setTheme("dark-theme");
-			} else {
-				this.setTheme("light-theme");
-			}
-		},
-		setTheme(theme) {
-			localStorage.setItem("user-theme", theme);
-			this.userTheme = theme;
-			document.documentElement.className = theme;
-		},
-		getMediaPreference() {
-			const hasDarkPreference = window.matchMedia(
-				"(prefers-color-scheme: light)"
-			).matches;
-			if (hasDarkPreference) {
-				return "dark-theme";
-			} else {
-				return "light-theme";
-			}
-		},
-	},
+  name: "Toggle",
+  components: {
+    VueScrollProgressBar,
+  },
+  mounted() {
+    const initUserTheme = this.getMediaPreference();
+    this.setTheme(initUserTheme);
+  },
+  data() {
+    return {
+      userTheme: "light-theme",
+    };
+  },
+  methods: {
+    toggleTheme() {
+      const activeTheme = localStorage.getItem("user-theme");
+      if (activeTheme === "light-theme") {
+        this.setTheme("dark-theme");
+      } else {
+        this.setTheme("light-theme");
+      }
+    },
+    setTheme(theme) {
+      localStorage.setItem("user-theme", theme);
+      this.userTheme = theme;
+      document.documentElement.className = theme;
+    },
+    getMediaPreference() {
+      const hasDarkPreference = window.matchMedia(
+        "(prefers-color-scheme: light)"
+      ).matches;
+      if (hasDarkPreference) {
+        return "dark-theme";
+      } else {
+        return "light-theme";
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
-@import "../Toggle/Toggle.style.scss"
+@import "../Toggle/Toggle.style.scss";
 </style>
